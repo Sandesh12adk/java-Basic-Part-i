d64e31dc5346fbd8331af09eedbc7a9edeb8e3b5
Sandesh adhikari <sandesh12adk@gmail.com>
2024-04-26 15:27:38 +0545

    Added file10

---
+++ src/First.java
@@ -0,0 +1,10 @@
+ /*1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+ Sandesh adhikari */
+ package com.example;
+public class First {
+    public static void main(string[] args){
+     ClassFirst.methodFIrst();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.example.First;
+public class Main {
+    public static void main(String[] args){
+      //  First.main();
+        First.main();
+    }
+}
